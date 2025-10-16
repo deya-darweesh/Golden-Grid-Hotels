89a14bbea476a5e87c32345250dc19d2ce47498c
Deya Darweesh <168779649+deya-darweesh@users.noreply.github.com>
2025-10-16 06:28:56 +0300

    render

--- GG_Hotels_project/settings.py
+++ GG_Hotels_project/settings.py
@@ -126,6 +126,40 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+import dj_database_url
+from pathlib import Path
+STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+
+
+MIDDLEWARE+= 'whitenoise.middleware.WhiteNoiseMiddleware',
+ALLOWED_HOSTS += ['.onrender.com','localhost','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
+
+
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=not DEBUG
+        )
+    }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),  # same name as your .env file
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',  
+        'PORT': '5432',
+    }
+}
 
 # Authentication redirect URLs
 LOGIN_REDIRECT_URL = 'homepage'
